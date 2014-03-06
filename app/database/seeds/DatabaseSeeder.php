c0ed4bdf1fec023b26d43ea25eb1e65f75e40186
Tan Chen <tanchen2014@gmail.com>
2014-03-06 12:29:09 +0900

    [db]adding-text-reading-for-seeding

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -27,12 +27,16 @@ class UserTableSeeder extends Seeder {
     {
         Eloquent::unguard();
 
-        User::create(array(
-            'name' => 'test1',
-            'user_name'=>'test1@gmail.com',
-            'password'=>'test1')
-        );
-
+//        $path = 'path/to/file';
+//        $samples = explode(PHP_EOL, rtrim(file_get_contents( File::get($path))));
+//
+//        foreach($samples as $sample) {
+            User::create(array(
+                'name' => 'test1',
+                'user_name'=>'test1@gmail.com',
+                'password'=>'test1')
+            );
+//        }
         // TODO: sneed a more generic way for data insertion, e.g. read from text file?
     }
 
