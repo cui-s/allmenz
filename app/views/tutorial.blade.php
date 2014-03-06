58adae7e210ab5eebd00d3fab970fbbcb239cdcf
Tan Chen <tanchen2014@gmail.com>
2014-03-06 12:38:21 +0900

    [design]getting-tutorial-page

---
+++ app/views/tutorial.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.base')
+
+@section('content')
+<div id="am-content">
+	<div id="am-index-category">
+        <div class="am-container">
+            <div class="am-tutorial-row text-center">
+                <p class="am-h0">ALLMENZへようこそ!
+                    <!-- <i class="fa fa-spinner fa-1x fa-spin"></i>-->
+                </p>
+            </div>
+            <div class="am-tutorial-row text-center">
+                <span class="am-h3">髪型か脚ケアまで、なんでも、こたえをみつけろう。髪型か脚ケアまで、なんでも、こたえをみつけろう、髪型か脚ケアまで、なんでも、こたえをみつけろう、髪型か脚ケアまで、なんでも、こたえをみつけろう</span>
+            </div>
+            <div class="am-tutorial-row text-center">
+            	<span class="am-h3">我々は他のサイトからの少し異なっている。ここではどのようにです</span>
+                <button type="button" class="btn btn-primary btn-lg">
+                    <span class="glyphicon glyphicon-circle-arrow-down"></span>
+                </button>     
+            </div>                    
+        </div>
+    </div>
+
+    <div id="am-index-tutorial">
+        <div class="am-container">
+            <div class="am-row text-center"><img><img></div>
+            <div class="am-row text-center">
+                <p class="am-h0">質問を答え、ない気晴らしを得る
+                     <!-- <i class="fa fa-spinner fa-1x fa-spin"></i>-->
+                </p>
+            </div>
+            <div class="am-row text-center"><img><img></div>         
+            <div class="am-row text-center"><img><img></div>                    
+        </div>
+    </div>
+
+    <div id="am-index-category">
+        <div class="am-container">
+            <div class="am-row text-center"><img><img></div>
+            <div class="am-row text-center">
+                <p class="am-h0">特色トピックを得るために適用
+                    <!-- <i class="fa fa-spinner fa-1x fa-spin"></i>-->
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div id="am-index-tutorial">
+        <div class="am-container">
+            <div class="am-row text-center"><img><img></div>
+            <div class="am-row text-center">
+                <p class="am-h0">自分の投稿に投票するときに評価を得る
+                    <!-- <i class="fa fa-spinner fa-1x fa-spin"></i>-->
+                </p>
+            </div>
+            <div class="am-row text-center"><img><img></div>       
+            <div class="am-row text-center"><img><img></div>                      
+        </div>
+    </div>
+
+    <div id="am-index-category">
+        <div class="am-container">
+            <div class="am-row text-center"><img><img></div>
+            <div class="am-row text-center">
+                <p class="am-h0">特別な成果のためのバッジ、そしてフラエのロックを解除
+                    <!-- <i class="fa fa-spinner fa-1x fa-spin"></i>-->
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div id="am-index-tutorial">
+        <div class="am-container">
+            <div class="am-tutorial-back text-center">
+                <p class="am-h0">ただいま！
+                    <!-- <i class="fa fa-spinner fa-1x fa-spin"></i>-->
+                </p>
+            </div>
+            <div class="am-tutorial-content">
+                <div class="am-row text-center">
+                    <div style="width: 50%; float: left">
+                        <button type="button" class="am-tutorial-btn1">
+                            <span> 質問を作り</span>
+                        </button>  
+                    </div>
+                    <div style="width: 50%; float: right">
+                        <button type="button" class="am-tutorial-btn1">
+                            <span> 質問を答え</span>
+                        </button> 
+                    </div>
+                </div>    
+            </div>
+            <div class="am-tutorial-site-center text-center">
+                <span>サイト上で、より詳細な情報をお探しですか？　</span>
+                <button type="button" class="am-tutorial-btn2">
+                        <span> ヘルプセンータへ </span>
+                </button> 
+            </div>             
+        </div>
+    </div>
+</div>
+
+@stop
